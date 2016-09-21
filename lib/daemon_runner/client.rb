a0c74c853fe6254f2ad555e20c25fd0ac986d3ef
Andrew Thompson <Andrew_Thompson@rapid7.com>
2016-09-21 13:50:13 -0400

    Split out the details of running a task
    This also fixes an issue where the class name was not correct if we were
    not given a class instance.

--- lib/daemon_runner/client.rb
+++ lib/daemon_runner/client.rb
@@ -66,17 +66,7 @@ module DaemonRunner
       loop do # Loop on tasks
         logger.warn 'Tasks list is empty' if tasks.empty?
         tasks.each do |task|
-          instance = task[0]
-          method = task[1]
-          args = task[2..-1].flatten
-          if args.empty?
-            logger.debug "Running #{instance.class}.#{method}"
-            out = instance.send(method.to_sym)
-          else
-            logger.debug "Running #{instance.class}.#{method}(#{args})"
-            out = instance.send(method.to_sym, args)
-          end
-          logger.debug "Got: #{out}"
+          run_task(task)
           sleep 1
         end
 
@@ -89,6 +79,49 @@ module DaemonRunner
 
       sleep error_sleep_time
       retry
+    end
+
+    private
+
+    # @private
+    # @param [Array] task task to parse
+    # @return [Array<String, String, Array>] task parsed in parts: Class, method, and arguments
+    def parse_task(task)
+      raise ArgumentError, 'Not enough elements in the Array' if task.length < 2
+      out = {}
+      out[:instance] = task[0]
+
+      out[:class_name] = if out[:instance].class.to_s == 'Class'
+                           out[:instance].name.to_s
+                         else
+                           out[:instance].class.to_s
+                         end
+
+      out[:method] = task[1]
+      out[:args] = task[2..-1].flatten
+      out
+    end
+
+    # @private
+    # @param [Array<String, String, Array>] task to run
+    # @return [String] output returned from task
+    def run_task(task)
+      parsed_task = parse_task(task)
+      instance = parsed_task[:instance]
+      class_name = parsed_task[:class_name]
+      method = parsed_task[:method]
+      args = parsed_task[:args]
+      log_line = "Running #{class_name}.#{method}"
+      log_line += "(#{args})" unless args.empty?
+      logger.debug log_line
+
+      out = if args.empty?
+              instance.send(method.to_sym)
+            else
+              instance.send(method.to_sym, args)
+            end
+      logger.debug "Got: #{out}"
+      out
     end
   end
 end
--- test/daemon_runner_test.rb
+++ test/daemon_runner_test.rb
@@ -1,11 +1,50 @@
 require 'test_helper'
 
+class MyService; end
+class MyService::Tasks; end
+class MyService::Tasks::Foo; end
+class MyService::Tasks::Bar; end
+class MyService::Tasks::Baz; end
+
+class TestDaemonRunner < ::DaemonRunner::Client
+end
+
 class DaemonRunnerTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::DaemonRunner::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_parsed_task_can_get_class_name
+    runner = TestDaemonRunner.new({})
+    task = [::MyService::Tasks::Foo.new, 'run!']
+    parsed_task = runner.send(:parse_task, task)
+    assert_equal 'MyService::Tasks::Foo',
+                 parsed_task[:class_name]
+  end
+
+  def test_parsed_task_can_get_module_name
+    runner = TestDaemonRunner.new({})
+    task = [::MyService::Tasks::Foo, 'run!']
+    parsed_task = runner.send(:parse_task, task)
+    assert_equal 'MyService::Tasks::Foo',
+                 parsed_task[:class_name]
+  end
+
+  def test_parsed_task_can_get_method_name
+    runner = TestDaemonRunner.new({})
+    task = [::MyService::Tasks::Foo, 'run!']
+    parsed_task = runner.send(:parse_task, task)
+    assert_equal 'run!', parsed_task[:method]
+  end
+
+  def test_parsed_task_can_get_args
+    runner = TestDaemonRunner.new({})
+    task1 = [::MyService::Tasks::Foo, 'run!', 'foo']
+    parsed_task1 = runner.send(:parse_task, task1)
+    assert_equal ['foo'], parsed_task1[:args]
+
+    task2 = [::MyService::Tasks::Foo, 'run!', 'foo', 'bar']
+    parsed_task2 = runner.send(:parse_task, task2)
+    assert_equal ['foo', 'bar'], parsed_task2[:args]
   end
 end
