9701d0b6969dd8e460e42a8ad09b9961352db2ff
Andrew Thompson <Andrew_Thompson@rapid7.com>
2016-09-27 14:49:05 -0400

    Simplify shell-out options a bit

--- daemon_runner.gemspec
+++ daemon_runner.gemspec
@@ -27,6 +27,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "logging", "~> 2.1"
+  spec.add_dependency "mixlib-shellout", "~> 2.2"
 
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/daemon_runner/shell_out.rb
+++ lib/daemon_runner/shell_out.rb
@@ -3,25 +3,54 @@ require 'mixlib/shellout'
 module DaemonRunner
   class ShellOut
     attr_reader :runner, :stdout
+    attr_reader :command, :cwd, :timeout
 
-    def cwd
-      '/tmp'
+    # @param command [String] the command to run
+    # @param cwd [String] the working directory to run the command
+    # @param timeout [FixNum] the command timeout
+    def initialize(command: nil, cwd: '/tmp', timeout: 15)
+      @command = command
+      @cwd = cwd
+      @timeout = timeout
     end
 
-    def timeout
-      15
-    end
-
-    def runner
-      @runner ||= Mixlib::ShellOut.new(command, :cwd => cwd, :timeout => timeout)
-    end
-
+    # Run command
+    # @return [Mixlib::ShellOut] client
     def run!
+      validate_args
       runner
       @runner.run_command
       @runner.error!
       @stdout = @runner.stdout
       @runner
     end
+
+    private
+
+    # @private
+    # Validate arguments before trying to start the command
+    # @ raise [ArgumentError] if any of the arguments are missing
+    def validate_args
+      if @command.nil? && !respond_to?(:command)
+        raise ArgumentError, 'Must pass a command or implement a command method'
+      end
+
+      if @cwd.nil? && !respond_to?(:cwd)
+        raise ArgumentError, 'Must pass a cwd or implement a cwd method'
+      end
+
+      if @timeout.nil? && !respond_to?(:timeout)
+        raise ArgumentError, 'Must pass a timeout or implement a timeout method'
+      end
+    end
+
+    # @private
+    # Setup a new Mixlib::ShellOut client runner
+    # @return [Mixlib::ShellOut] client
+    def runner
+      @runner ||= Mixlib::ShellOut.new(command, :cwd => cwd, :timeout => timeout)
+    end
+
+
   end
 end
